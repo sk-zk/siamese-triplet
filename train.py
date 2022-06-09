058c447f772b9090481dc95eda9a3aa13b075af5
sk-zk <sk-zk@users.noreply.github.com>
2022-06-09 11:58:35 +0200

    Use Imagenet mean and std

--- train.py
+++ train.py
@@ -29,13 +29,13 @@ class TripletDataset(Dataset):
                 transforms.Resize((before_crop, before_crop)),
                 transforms.RandomCrop(input_size),
                 transforms.ToTensor(),
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
         else:
             self.transform = transforms.Compose([
                 transforms.Resize((input_size, input_size)),
                 transforms.ToTensor(),
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
 
     def __get_class_imgs(self, class_dir, class_id):
